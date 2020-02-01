21bff63c1d0b45dc75e8c7f5da7c598a84e2ac4d
jakekandell <jakekandell03@gmail.com>
2020-02-01 02:02:54 -0800

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jakekandell/Documents/PythonProjects/quiz-site/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ quiz_site/quiz/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Quiz(models.Model):
+    quiz_title = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.quiz_title
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=300)
+    correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text

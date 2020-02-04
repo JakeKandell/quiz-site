8d2864e8d12a42d55f1be9a1dbff5a2aa6be7001
jakekandell <jakekandell03@gmail.com>
2020-02-04 01:43:27 -0800

    added urlconfs for quizzes

--- quiz_site/quiz/urls.py
+++ quiz_site/quiz/urls.py
@@ -2,6 +2,21 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'quiz'
 urlpatterns = [
+    # ex: /quiz/
     path('', views.index, name='index'),
+
+    # ex: /quiz/5/
+    path('<int:quiz_id>/', views.single_quiz, name='single_quiz'),
+
+    # ex: /quiz/5/3/
+    path('<int:quiz_id>/<int:question_id>/', views.single_question, name='single_question'),
+
+    path('<int:quiz_id>/<int:question_id>/vote/', views.vote, name='vote'),
+
+    # ex: /quiz/5/results/
+    path('<int:quiz_id>/results/', views.results, name='results'),
+
 ]

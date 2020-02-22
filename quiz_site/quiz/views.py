fbf3313140d4ee2bc5c3f7d0f78836f2abfeab7a
jakekandell <jakekandell03@gmail.com>
2020-02-21 22:41:34 -0800

    incorporated django sessions into website

--- quiz_site/quiz/views.py
+++ quiz_site/quiz/views.py
@@ -21,7 +21,14 @@ def index(request):
 def single_quiz(request, quiz_id):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
 
-    return render(request, 'quiz/single_quiz.html', {'quiz': quiz})
+    request.session["num_correct"] = 0
+    request.session["num_wrong"] = 0
+
+    context = {
+        'quiz': quiz,
+    }
+
+    return render(request, 'quiz/single_quiz.html', context)
 
 
 # Specific question
@@ -62,11 +69,26 @@ def vote(request, quiz_id, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        # selected_choice.votes += 1
-        # selected_choice.save()
+
+        # finds which choice is the correct answer
+        #for choice in current_question.choice_set.all():
+         #   if choice.correct == True:
+          #      correct_answer = choice
+        correct_answer = current_question.choice_set.get(correct=True)
+
+        if selected_choice == correct_answer:
+            print("You are right")
+            request.session["num_correct"] += 1
+        else:
+            print("You are wrong")
+            request.session["num_wrong"] += 1
+
+
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.    last_question_check = False
+
+        # checks if next page should be results or next question
         if question_id == (len(quiz.question_set.all())):
             return HttpResponseRedirect(reverse('quiz:results', args=(quiz.id,)))
         else :
@@ -76,4 +98,8 @@ def vote(request, quiz_id, question_id):
 
 # Quiz results
 def results(request, quiz_id):
-    return render(request, 'quiz/results.html')
+    context = {
+        'num_correct': request.session["num_correct"],
+        'num_wrong': request.session["num_wrong"],
+    }
+    return render(request, 'quiz/results.html', context)

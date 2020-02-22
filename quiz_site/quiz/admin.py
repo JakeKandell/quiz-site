edb107e1f8585de41064f63cbb3d9a453cb5a137
jakekandell <jakekandell03@gmail.com>
2020-02-22 02:37:18 -0800

    registered Question and Choice models

--- quiz_site/quiz/admin.py
+++ quiz_site/quiz/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Quiz
+from .models import Quiz, Question, Choice
 # Register your models here.
 
 admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Choice)

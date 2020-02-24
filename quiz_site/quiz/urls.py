7d000716afd1bb6c9302b7dd210b52505d8189a0
jakekandell <jakekandell03@gmail.com>
2020-02-23 20:55:28 -0800

    added url to create quizzes

--- quiz_site/quiz/urls.py
+++ quiz_site/quiz/urls.py
@@ -14,10 +14,13 @@ urlpatterns = [
     # ex: /quiz/5/3/
     path('<int:quiz_id>/<int:question_id>/', views.single_question, name='single_question'),
 
-
+    # ex: /quiz/5/3/vote/
     path('<int:quiz_id>/<int:question_id>/vote/', views.vote, name='vote'),
 
     # ex: /quiz/5/results/
     path('<int:quiz_id>/results/', views.results, name='results'),
+
+    # ex : /quiz/create/
+    path('create/', views.create_quiz, name='create_quiz'),
 
 ]

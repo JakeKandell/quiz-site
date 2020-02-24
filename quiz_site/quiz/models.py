98e5676dd9b578affddce22fc7335986966d85aa
jakekandell <jakekandell03@gmail.com>
2020-02-23 20:55:02 -0800

    added num_questions field to Quiz model

--- quiz_site/quiz/models.py
+++ quiz_site/quiz/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Quiz(models.Model):
     quiz_title = models.CharField(max_length=300)
+    num_questions = models.IntegerField(default=0)
 
     def __str__(self):
         return self.quiz_title

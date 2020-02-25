ee75b873d056801ebe00cb240978fdfdbcfa2503
jakekandell <jakekandell03@gmail.com>
2020-02-24 20:07:28 -0800

    added urlconf for creating question forms

--- quiz_site/quiz/urls.py
+++ quiz_site/quiz/urls.py
@@ -20,7 +20,10 @@ urlpatterns = [
     # ex: /quiz/5/results/
     path('<int:quiz_id>/results/', views.results, name='results'),
 
-    # ex : /quiz/create/
+    # ex: /quiz/create/
     path('create/', views.create_quiz, name='create_quiz'),
+
+    # ex: /quiz/create/7/2/
+    path('create/<int:quiz_id>/<int:question_id>/', views.create_question, name='create_question'),
 
 ]
